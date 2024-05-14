180d760cd0720a894db6f2f39178efa2875f4da0
Raphael <raphaelsoarescosta@gmail.com>
2024-05-14 15:41:37 -0300

    Update key AI

--- openai_api/client.py
+++ openai_api/client.py
@@ -1,11 +1,12 @@
 import os
 import google.generativeai as genai
 
+
 #Caso o usuário não preencha a Bio ao cadastrar novo carro, a bio é preenchida automaticamente atraves da IA
 
 #A chave API fica na variável de ambiente chamada.
 
-API_KEY = os.environ.get('API_KEY') 
+API_KEY = os.environ.get('AIzaSyBIDYA7i4IQCpb17e2992YN03dElGxd62w') 
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-pro')
@@ -16,3 +17,4 @@ def get_car_ai_bio(model_name, brand, year):
     response = model.generate_content(prompt)
 
     return response.text
+    
